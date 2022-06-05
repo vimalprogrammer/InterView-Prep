daa13fdda3d2bf5c76530b04c6a06dea10ffb85f
vimalprogrammer <msvimal10@gmail.com>
2022-06-05 16:01:33 +0530

    distinct elements in an array

--- O13DupElemArray.cpp
+++ O13DupElemArray.cpp
@@ -22,3 +22,36 @@ int main()
 	printDistinct(arr, n);
 	return 0;
 }
+
+
+//-----------Distinct Elements in an array------------//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printDistinct(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        int flag=1;
+        for(int j=0; j<n; j++)
+        {
+            if(arr[i] == arr[j] && i!=j)
+            {
+                flag=0;
+                break;
+            }
+        }
+        
+        if(flag)
+            cout<<arr[i]<<" ";
+    }
+}
+
+int main()
+{
+	int arr[] = {6, 5, 5, 10, 5, 4, 9, 120, 4, 6, 10};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	printDistinct(arr, n);
+	return 0;
+}
