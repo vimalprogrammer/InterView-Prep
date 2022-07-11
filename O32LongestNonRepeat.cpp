5a69b4d92ff4d7f49248dd07799c7c27740a9f0e
vimalprogrammer <msvimal10@gmail.com>
2022-07-11 21:40:56 +0530

    Longest Non Repeat Non Character

---
+++ O32LongestNonRepeat.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    
+int lengthOfLongestSubstring(string s) 
+{
+    int n=s.size();
+    unordered_map<char,int>mp;
+    int newStart=0,longnonRep=0;
+    for(int i=0;i<n;i++)
+    {
+        if(mp[s[i]]!=0)
+            newStart=max(newStart,mp[s[i]]+1);
+        mp[s[i]]=i;
+        
+        longnonRep=max(longnonRep,i-newStart+1);
+    }
+    return longnonRep;
+}
+
+int main()
+{
+    int res=lengthOfLongestSubstring("vimalvarshu");//Output = 7 --> lvarshu
+    cout<<res;
+}
